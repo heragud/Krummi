b9701b66148ae2dbe18e9d7e6f24132087ad8ca0
Hera Gudlaugsdottir <hera@hi.is>
2017-09-14 15:34:51 +0100

    latest edition, the old one is wrong

--- CTCS_example.py
+++ CTCS_example.py
@@ -5,29 +5,22 @@ def initialBell(x):
     return np.where(x%1.<0.5, np.power(np.sin(2*x*np.pi),2),0)
 
 nx = 40
-c = 0.2
+c = 1
 x = np.linspace(0.0,1.0,nx+1)
 phi=initialBell(x)
-phiNew=phi.copy()
+
 phiOld=phi.copy()
-
-for j in xrange(1,nx):
-    phi[j] = phiOld[j] - 0.5*c*(phiOld[j+1] - phiOld[j-1])
-
-phi[0] = phiOld[0] - 0.5*c*(phiOld[1] - phiOld[nx-1])
-phi[nx] = phi[0]
 
 nt = 40
 
-for n in xrange(1,nt):
-    for j in xrange(1,nx):
-        phiNew[j] = phiOld[j] - c*(phi[j+1] - phi[j-1])
+for n in xrange(0,nt):
+    for j in xrange(0,nx):
+        phi[j] = phiOld[j] - 0.5*c*(phiOld[j+1] - phiOld[j-1])
     
-    phiNew[0] = phiOld[0] - c*(phi[1] - phi[nx-1])
-    phiNew[nx] = phiNew[0]
+    phi[0] = phiOld[0] - 0.5*c*(phiOld[1] - phiOld[nx-1])
+    phi[nx] = phi[0]
     
     phiOld = phi.copy()
-    phi = phiNew.copy()
     
 u = 1.
 dx = 1./nx
@@ -35,10 +28,9 @@ dt = c*dx/u
 t = nt*dt
 
 plt.plot(x, initialBell(x - u*t), 'k', label='analytic')
-plt.plot(x,phi,'b',label='CTCS')
+plt.plot(x,phi,'b',label='FTCS')
 plt.legend(loc='best')
 plt.xlabel('x')
 plt.ylabel('$\phi$')
 plt.axhline(0,linestyle=':',color='black')
 plt.show()
-
